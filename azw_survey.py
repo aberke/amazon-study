6d11a59984f38f80c302eb663fb208c84fd280ef
Dan Calacci <dcalacci@media.mit.edu>
2022-10-04 19:55:59 -0400

    Command line tool for auto-paying turkers.
    includes commands for:
    - saving qualtrics data to CSV
    - saving "assignment" data from mturk to csv
    - dry-run payment of workers, including bonuses
    - automatic payment of workers through the API
    - ensuring bonuses are not over $0.50
    
    additional little features:
    - configuring the qualtrics survey ID and aws credentials using a JSON
      file

--- azw_survey.py
+++ azw_survey.py
@@ -6,6 +6,7 @@ Usage:
   azw_survey.py qualtrics get_responses [--keep-upload-rows] [--start-date DATE] [--end-date DATE] (-o FILE)
   azw_survey.py mturk get_hits [--status STATUS] [-o FILE]
   azw_survey.py mturk get_assignments HIT_ID [--status STATUS] (-o FILE)
+  azw_survey.py mturk pay_turkers HIT_ID [--dry-run|--autopay] [-o FILE]
   azw_survey.py (-h | --help)
   azw_survey.py --version
 
@@ -18,6 +19,16 @@ Options:
   --end-date DATE           Only get responses before this end date
   --keep-upload-rows        Keep upload file rows (Q43)
   --status STATUS           Only show mturk assignments that are STATUS ('Submitted', 'Approved', or 'Rejected'). [default: Submitted]
+  --dry-run                 Dry run payment. Returns amounts for review.
+  --autopay                 Actually pay / approve assignments
+
+Examples:
+
+Get all qualtrics responses and save to file:
+    ./azw_survey.py qualtrics get_responses -o qualtrics_export.csv
+
+Pay turkers for HIT_ID and save record to `pay_record.csv`:
+    ./azw_survey.py mturk pay_turkers HIT_ID -o pay_record.csv --autopay
 """
 from docopt import docopt
 from mturk_qualtrics import qualtrics
@@ -25,33 +36,62 @@ from mturk_qualtrics.hits import HITUtils
 import pandas as pd
 import json
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = docopt(__doc__, version="AZW Survey 0.1")
     with open(args["--config"]) as f:
         config = json.load(f)
 
-    if (args['qualtrics']):
-        df = qualtrics.get_survey_responses(config = config['qualtrics'], 
-                                            drop_uploads = not args['--keep-upload-rows'])
+    if args["qualtrics"]:
+        df = qualtrics.get_survey_responses(
+            config=config["qualtrics"], drop_uploads=not args["--keep-upload-rows"]
+        )
         print("> Got survey responses. Data preview:")
         print(df)
         print(f"> Writing to csv {args['-o']}...")
-        df.to_csv(args['-o'])
+        df.to_csv(args["-o"])
         print("> Written to disk! Exiting.")
-    elif args['mturk']:
-        hits = HITUtils(config['aws'])
+    elif args["mturk"]:
+        hits = HITUtils(config["aws"])
         # hits.initialize_client(config['aws'])
-        if (args['get_hits']):
+        if args["get_hits"]:
             hits = hits.get_amazon_survey_HITs()
             print("Assignments:")
-            print(pd.DataFrame(hits)[['CreationTime', 'HITId', 'Title']])
-            if (args['-o']):
-                pd.DataFrame(hits).to_csv(args['-o'])
-        elif args['get_assignments']:
+            print(pd.DataFrame(hits)[["CreationTime", "HITId", "Title"]])
+            if args["-o"]:
+                pd.DataFrame(hits).to_csv(args["-o"])
+        elif args["get_assignments"]:
             print(f"> Getting assignments for HIT {args['HIT_ID']}")
-            assignments = hits.get_assignments_for_HIT(args['HIT_ID'])
+            assignments = hits.get_assignments_for_HIT(args["HIT_ID"])
             print(f"> Found {len(assignments)} Assignments:")
             print(pd.DataFrame(assignments))
-            if args['-o']:
+            if args["-o"]:
                 print(f"> Saving to {args['-o']}...")
-                pd.DataFrame(assignments).to_csv(args['-o'])
+                pd.DataFrame(assignments).to_csv(args["-o"])
+        elif args["pay_turkers"]:
+            print(f"> Getting qualtrics responses...")
+            qualtrics_df = qualtrics.get_survey_responses(
+                config=config["qualtrics"], drop_uploads=not args["--keep-upload-rows"]
+            )
+            print(f"> Getting assignments for HIT {args['HIT_ID']}")
+            worker_assignments = hits.get_worker_assignment_data(
+                args["HIT_ID"], qualtrics_df
+            )
+            assn_df = pd.DataFrame(worker_assignments)
+            print("> Got matched worker assignments and bonus info:")
+            print(assn_df)
+            if args["-o"] and not args["--dry-run"] and not args["--autopay"]:
+                print(f"> Saving to file {args['-o']}")
+                assn_df.to_csv(args["-o"])
+            if args["--dry-run"]:
+                pay_record = hits.pay_workers(worker_assignments, dry_run=True)
+                pay_df = pd.DataFrame(pay_record)
+                print("> Dry Run Payment:")
+                print(pay_df)
+                pay_df.to_csv(args["-o"])
+            elif args["--autopay"]:
+                print("> Paying workers for real...")
+                pay_record = hits.pay_workers(worker_assignments, dry_run=False)
+                pay_df = pd.DataFrame(pay_record)
+                print("> REAL Payment record:")
+                print(pay_df)
+                pay_df.to_csv(args["-o"])
